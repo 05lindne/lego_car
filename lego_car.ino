5371782e23debb1376f146e6ba596e1ff97ae05e
05lindne <05lindne@hotmail.com>
2020-06-26 16:03:00 +0200

    Update servo

--- lego_car.ino
+++ lego_car.ino
@@ -13,14 +13,16 @@ https://www.yumpu.com/de/document/read/51446269/lego-9v-technic-motors-compared-
 // Include IR Remote Library by Ken Shirriff
 #include <IRremote.h>
 
+#include <Servo.h>
+
 // Define sensor pin
 const int RECV_PIN = 4;
 // Motor on digital pin 9
 const int MOTOR = 9;
-// Steering motor on digital pin 8
-const int STEER = 7;
 // Define LED pin for testing purposes	
 const int ledPin = 13;
+// Define pin for servo signal
+const int servoPin = 10;
 
 // Define integer to remember toggle state
 int togglestate = 0;
@@ -29,54 +31,53 @@ int togglestate = 0;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
+// Call constructor for Servo class
+Servo myServo;
+
+
 void setup()
 {
 	// Enable the IR Receiver
 	irrecv.enableIRIn();
 	// Set motor pin as output
 	pinMode(MOTOR, OUTPUT);
-	// Set steering motor pin as output
-	pinMode(STEER, OUTPUT);
+	// Set servo motor pin
+	myServo.attach(servoPin);
 	// Set LED pin as Outputs
 	pinMode(ledPin, OUTPUT);
+
 }
 
 void loop()
 {
 	if (irrecv.decode(&results)){
 
-        switch(results.value){
+		switch(results.value){
    
-        	case 0xFF18E7: // remote control button 2
-        	// Toggle motor On or Off
-        	if(togglestate==0){
-        		digitalWrite(ledPin, HIGH);
+			case 0xFF18E7: // remote control button 2
+			// Toggle motor On or Off
+			if(togglestate==0){
+				digitalWrite(ledPin, HIGH);
 				analogWrite(MOTOR, 200);
 				delay(10);
 				togglestate = 1;
 			}
 			else {
 				digitalWrite(ledPin, LOW);
-        		analogWrite(MOTOR, 0);
+				analogWrite(MOTOR, 0);
 				delay(10);
 				togglestate = 0;
-        	}
-			break;
-// marry this code with the Servo example to make motor turn in both directions
-			case 0xFF10EF: // remote control button 4
-				analogWrite(STEER, 200);
-				// digitalWrite(ledPin, HIGH);
-				delay(200);
-				analogWrite(STEER, 0);
-				// digitalWrite(ledPin, LOW);
+			}
 			break;
 
-			case 0xFF38C7: // remote control button 6
-				analogWrite(STEER, -200);
-				// digitalWrite(ledPin, HIGH);
-				delay(200);
-				analogWrite(STEER, 0);
-				// digitalWrite(ledPin, LOW);
+			case 0xFF10EF: // remote control button 4
+				myServo.write(45);
+				delay(15);
+			break;
+
+			case 0xFF5AA5: // remote control button 6
+				myServo.write(135);
+				delay(15);
 			break;
 		}
 		irrecv.resume();
