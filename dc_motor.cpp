ed9ab4e22fcf982a3d301ba20d10d2627c5b3d04
05lindne <05lindne@hotmail.com>
2020-10-23 15:29:24 +0200

    started putting code into classes

---
+++ dc_motor.cpp
@@ -0,0 +1,10 @@
+/*
+dcMotor.cpp Source Code for library to control a DC motor.
+by Sarah Lindner
+created 10/23/2020
+
+*/
+dcMotor::dcMotor(int pin)
+{
+	this.pin = pin;
+}
---
+++ dc_motor.h
@@ -0,0 +1,20 @@
+/*
+dcMotor.h Library to control a DC motor.
+by Sarah Lindner
+created 10/23/2020
+
+*/
+
+#ifndef dcMotor_h
+#define dcMotor_h
+
+class dcMotor
+{
+	public:
+		dcMotor(int pin);
+	private:
+		int pin;
+}
+
+
+#endif dcMotor_h
